799215a4b637fd7c5e7aa20a4e5ec3898806d097
fly <fly@load-page.com>
2017-06-02 14:26:44 +0800

    rename function validator to censor add CensorException

--- src/Validation/ValidatesRequests.php
+++ src/Validation/ValidatesRequests.php
@@ -1,27 +1,25 @@
 <?php
 namespace Addons\Censor\Validation;
 
-use Closure;
 use Addons\Censor\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Foundation\Validation\ValidatesRequests as BaseValidatesRequests;
-use Lang;
+use Addons\Censor\Exceptions\CensorException;
+use Illuminate\Contracts\Validation\Validator;use Illuminate\Foundation\Validation\ValidatesRequests as BaseValidatesRequests;
 
 trait ValidatesRequests
 {
 	use BaseValidatesRequests;
 
-	public function getValidatorScript($censorKey, $attributes, Model $model = null)
+	public function censorScripts($censorKey, $attributes, Model $model = null)
 	{
 		$censor = $this->getCensorFactory()->make($censorKey, $attributes, $model);
 
 		return $censor->js();
 	}
+
 	/**
-	 * if json or api, use validateWithApi.
-	 * otherwish use validateWithNative
+	 * censor a 
 	 * 
 	 * @param  Request $request
 	 * @param  string  $censorKey
@@ -29,25 +27,7 @@ trait ValidatesRequests
 	 * @param  Model|null $model
 	 * @return array|Exception
 	 */
-	public function autoValidate(Request $request, $censorKey, $attributes, Model $model = null)
-	{
-		if ($request->expectsJson() || $request->offsetExists('of') || (!empty($request->route()) && in_array('api', $request->route()->gatherMiddleware())))
-			return $this->validateWithApi($request, $censorKey, $attributes, $model);
-		else 
-			return $this->validateWithNative($request, $censorKey, $attributes, $model);
-	}
-
-	/**
-	 * laravel's native validator
-	 * flash and back page when fails
-	 * 
-	 * @param  Request    $request
-	 * @param  string     $censorKey
-	 * @param  array      $attributes
-	 * @param  Model|null $model
-	 * @return array|Exception
-	 */
-	public function validateWithNative(Request $request, $censorKey, $attributes, Model $model = null)
+	public function censor(Request $request, $censorKey, $attributes, Model $model = null)
 	{
 		$censor = $this->getCensorFactory()->make($censorKey, $attributes, $model)->data($request->all());
 		$validator = $censor->validator();
@@ -55,32 +35,17 @@ trait ValidatesRequests
 	}
 
 	/**
-	 * api's validator
-	 * OutputException when fails
-	 * 
-	 * @param  Request    $request
-	 * @param  string     $censorKey
-	 * @param  array      $attributes
-	 * @param  Model|null $model
-	 * @return array|Exception
-	 */
-	public function validateWithApi(Request $request, $censorKey, $attributes, Model $model = null)
-	{
-		$censor = $this->getCensorFactory()->make($censorKey, $attributes, $model)->data($request->all());
-		$validator = $censor->validator();
-		return $validator->fails() ? $this->throwValidationOutputResponse($validator->errors()) : $censor->validData();
-	}
-
-	private function throwValidationOutputResponse(\Illuminate\Support\MessageBag $messagebag)
-	{
-		$errors = $messagebag->toArray();
-		$messages = [];
-		foreach ($errors as $lines) {
-			foreach ($lines as $message) {
-				$messages[] = trans(Lang::has('validation.post_fields_invalid.list') ? 'validation.post_fields_invalid.list' : 'core::common.validation.post_fields_invalid.list', compact('message'));
-			}
-		}
-		return $this->failure_post(false, ['errors' => $errors, 'messages' => implode($messages)], true);
+     * Throw the failed validation exception.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws Addons\Censor\Exceptions\CensorException
+     */
+    protected function throwValidationException(Request $request, $validator)
+    {
+		throw new CensorException($request, $validator);
 	}
 
 	/**
